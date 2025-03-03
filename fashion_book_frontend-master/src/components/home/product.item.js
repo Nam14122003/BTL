0a713218ef7ad92372cc7e83acdbacbe76a03ece
nam.ldn <nam.ldn@tinasoft.com.vn>
2025-03-03 22:39:50 +0700

    fix/discount

--- fashion_book_frontend-master/src/components/home/product.item.js
+++ fashion_book_frontend-master/src/components/home/product.item.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
-const ProductItem = ({urlImg, price, name, id, book }) => (
+import '../../styles/product.item.css'
+
+const ProductItem = ({ urlImg, price, name, id, book }) => (
     <div className="col-sm-4">
         <div className="product-image-wrapper">
             <div className="single-products">
-                <div className="productinfo text-center"
-                >   
-                    <Link to={'/product/' + id}><img src={urlImg} alt="" /></Link>
-                    <h4 className='name-product'>{name}</h4>
-                    <div className='product-content'>
-                    <h2>Giá:</h2>
-                    <Link to={'/product/' + id}><h2>{new Intl.NumberFormat('de-DE', {currency: 'EUR' }).format(price)}<sup>đ</sup></h2></Link>
+                <div className="productinfo text-center">
+                    <Link to={'/product/' + id}><img src={urlImg} alt="" className="product-image" /></Link>
+                    <h4 className="name-product">{name}</h4>
+                    <div className="product-content">
+                        {book.discount > 0 ? (
+                            <Fragment>
+                                <div className="price-wrapper">
+                                    <span className="original-price">
+                                        Giá gốc: <span className="strikethrough">
+                                            {new Intl.NumberFormat('de-DE').format(price)}<sup>đ</sup>
+                                        </span>
+                                    </span>
+                                    <span className="discounted-price">
+                                        Giá bán: {new Intl.NumberFormat('de-DE').format(book.discount)}<sup>đ</sup>
+                                    </span>
+                                    <span className="discount-badge">
+                                        -{Math.round(((price - book.discount) / price) * 100)}%
+                                    </span>
+                                </div>
+                            </Fragment>
+                        ) : (
+                            <Link to={'/product/' + id}>
+                                <h2>Giá: {new Intl.NumberFormat('de-DE').format(price)}<sup>đ</sup></h2>
+                            </Link>
+                        )}
                     </div>
-                    
-                    
-                  
                 </div>
             </div>
         </div>
     </div>
 )
+
 export default ProductItem
