ade7ead0fb3f9a145307b64d872b130944a45ca4
Nam14122003 <147422976+Nam14122003@users.noreply.github.com>
2025-05-21 07:50:36 +0700

    feat/ads

--- fashion_book_frontend-master/src/components/home/home.js
+++ fashion_book_frontend-master/src/components/home/home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import HeaderTop from '../header/header.top'
 import HeaderMiddle from '../header/header.middle'
 import HeaderBottom from '../header/header.bottom'
@@ -32,46 +32,84 @@ const Home = ({
   history,
   searchTextSubmit,
   addToCart
-}) => (
-  <div>
-    <header id="header">
-      <HeaderMiddle
-        islogin={islogin}
-        logout={() => logout()}
-        history={history}
-        sortType={sortType}
-        setSortType={(value) => setSortType(value)}
-        setSearchText={(value) => setSearchText(value)}
-        searchTextSubmit={() => searchTextSubmit()}
+}) => {
+  const [showAd, setShowAd] = useState(true);
+
+  return (
+    <div>
+      <header id="header">
+        <HeaderMiddle
+          islogin={islogin}
+          logout={() => logout()}
+          history={history}
+          sortType={sortType}
+          setSortType={(value) => setSortType(value)}
+          setSearchText={(value) => setSearchText(value)}
+          searchTextSubmit={() => searchTextSubmit()}
+        />
+        <HeaderBottom />
+      </header>
+      {showAd && (
+        <div style={{ position: 'relative', margin: '20px 0' }}>
+          <button
+            onClick={() => setShowAd(false)}
+            style={{
+              position: 'absolute',
+              top: 4,
+              right: 4,
+              background: 'rgba(0,0,0,0.5)',
+              color: '#fff',
+              border: 'none',
+              borderRadius: '50%',
+              width: 18,
+              height: 18,
+              cursor: 'pointer',
+              fontWeight: 'bold',
+              fontSize: 12,
+              zIndex: 2,
+              lineHeight: '14px',
+              padding: 0
+            }}
+            aria-label="Đóng quảng cáo"
+          >
+            ×
+          </button>
+          <a href="https://vrchallenge.io/" target="_blank" rel="noopener noreferrer" style={{ display: 'block', width: '100%', height: 70 }}>
+            <img
+              src="https://vrchallenge.io/wp-content/uploads/2025/04/nohu.gif"
+              alt="Quảng cáo"
+              style={{ width: '100vw', maxWidth: '100%', height: 70, objectFit: 'cover', display: 'block' }}
+            />
+          </a>
+        </div>
+      )}
+      <ContentHome
+        category={category}
+        publisher={publisher}
+        book={book}
+        totalpage={totalpage}
+        backPage={() => backPage()}
+        nextPage={() => nextPage()}
+        setPage={(page) => setPage(page)}
+        page={page}
+        setRangeType={(range) => setRangeType(range)}
+        title={title}
+        setTitle={(title) => setTitle(title)}
+        setBranch={(branch) => setBranch(branch)}
+        branch={branch}
+        author={author}
+        setIDBranch={(id) => setIDBranch(id)}
+        branchClick={(branch, id) => branchClick(branch, id)}
+        addToCart={(product) => addToCart(product)}
       />
-      <HeaderBottom />
-    </header>
-    <ContentHome
-      category={category}
-      publisher={publisher}
-      book={book}
-      totalpage={totalpage}
-      backPage={() => backPage()}
-      nextPage={() => nextPage()}
-      setPage={(page) => setPage(page)}
-      page={page}
-      setRangeType={(range) => setRangeType(range)}
-      title={title}
-      setTitle={(title) => setTitle(title)}
-      setBranch={(branch) => setBranch(branch)}
-      branch={branch}
-      author={author}
-      setIDBranch={(id) => setIDBranch(id)}
-      branchClick={(branch, id) => branchClick(branch, id)}
-      addToCart={(product) => addToCart(product)}
-    />
-    <footer id="footer">
-      <FooterTop />
-      <FooterMiddle />
-      <FooterBottom />
-    </footer>
-    <ChatBot />
-  </div>
-);
+      <footer id="footer">
+        <FooterTop />
+        <FooterMiddle />
+        <FooterBottom />
+      </footer>
+      <ChatBot />
+    </div>
+  );
+};
 
 export default Home
